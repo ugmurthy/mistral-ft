7f078b00563cce0a8bae13621eb5eecde19294c4
Murthy Udupa <ugmurthy@gmail.com>
2024-06-25 20:39:00 +0530

    new approach to streaming via askm

--- app/routes/_layout.askm.tsx
+++ app/routes/_layout.askm.tsx
@@ -1,84 +1,120 @@
-import type { LoaderFunction, LoaderFunctionArgs, SerializeFrom } from "@remix-run/node";
-import { defer, json } from "@remix-run/node";
-import { Await, useLoaderData, useNavigation, } from "@remix-run/react";
-import { Suspense, useEffect, useState } from "react";
-import {chat } from '../api/ollama.server';
-import ChatData from "~/components/ChatData";
+// app/routes/chat.tsx
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { mistralChat } from "~/api/mistralAPI.server";
+import _ from 'lodash';
 import Prompt from "~/components/Prompt";
-import CommandCopy from "~/components/CommandCopy";
+import { useLoaderData } from "@remix-run/react";
 
 function getURLdetails(request:Request) {
 	
-    const url = new URL(request.url);
-    if (url.pathname !== '/favicon.ico') { 
-        const role = url.searchParams.get("role");
-        const prompt= url.searchParams.get("prompt");
-        const remember =url.searchParams.get("remember")
-        return {prompt,role,remember}
-}
+  const url = new URL(request.url);
+  if (url.pathname !== '/favicon.ico') { 
+      const role = url.searchParams.get("role");
+      const prompt= url.searchParams.get("prompt");
+      const remember =url.searchParams.get("remember")
+      return {prompt,role,remember}
+  }
 } 
 
-
-
-export const loader:LoaderFunction = async ({request}:LoaderFunctionArgs )=>{
-    const {prompt,role,remember} = getURLdetails(request);
-    
-    if (!(role && prompt)) {
-      return defer({dataPromise:{},prompt:""})
+export async function loader({ request }: LoaderFunctionArgs) {
+  function chunks2Array(chunk) {
+    function getStr(c) {
+      if (c!=="") 
+        try {
+        return JSON.parse(c);
+        } catch(e) {
+          console.log("Error while parsing");
+          console.log("chunks2Arrat : ",c)
+          return {}
+        }
+     }
+    // return an array of json objects
+    // check if chunk has json objects, remove non-json objects from string
+    const retval = chunk.split('\n').map((c)=>c.substring(_.indexOf(c,"{"),_.lastIndexOf(c,"}")+1))
+    //console.log(retval)
+    if (retval[retval.length-1]==="") {
+      retval.pop();
     }
-    const addInstruct = ".Always ENSURE all output is in markdown format and provide titles, numbered subtitled, emphasis."
-    const modifiedPrompt = prompt +addInstruct // ++  depending on what we expecting
+    const objArray = retval.map(getStr)
+    //console.log("Chunks2Array: ",objArray);
+    return _.compact(objArray)
+  }
+
+  function array2Content(data) {
+    let result='';
+    for (const chunk of data) {
+        result = result + chunk.choices[0].delta.content;
+    }
+    return result
+    
+}
+  const {prompt,role,remember} = getURLdetails(request);
+
+    const modifiedPrompt = prompt // ++  depending on what we expecting
     const user = [{role:"user",content:modifiedPrompt}]
-    const sysPrompt = "You are an expert Marathon Coach. Your knowledge encompasses all allied fields related to running such as Nutrition, strength and mental training,musculoskeletal system, ability to motivate athletes, and racing strategy. Respond to questions and or topic related to Running. Politely refuse to answer other questions."
     //@TODO : get a prompt for 'system' depending on 'role' for now it is null
-    const system = [{role:"system",content:sysPrompt}]; //@TODO
+    const system = [{role:"system",content:"You are a world class Marathon Coach. Respond to questions and or topic related to Running. Politely refuse to answer other questions"}]; //@TODO
     const messages = [...system, ...user]
 
-    const response = await mistralChat(role,messages,false);
-    /*const data = await response.json();
-    return {data,prompt} */
-    
-    const dataPromise = response.json();
-    return defer({dataPromise,prompt})
-}
+    //1 const ret_val = await chat(model,messages,false);
+    //2 return {data:ret_val,prompt};
 
-export default function Component(){
-const {dataPromise,prompt} = useLoaderData<typeof loader>(); 
+    const response = await mistralChat(role,messages,true);
 
-if (prompt==="") {
-  return (
-    <Prompt aiRole='Coach'/>
-  )
-}
-return (
-  <div>
-    {prompt!="" 
-        ? 
-        <div className="p=10">
-          <div className="chat chat-start m-4">
-              <div className="avatar chat-image">
-                <div className="w-10 rounded-full">
-                  <img src="avatar.png"alt="avatar"/>
-                </div>
-              </div>
-              <div className="chat-bubble">{prompt} </div>
-              <CommandCopy txt={prompt} btnTxt="Copy"></CommandCopy>
-            </div>
-          <div className="p-2 pl-4 font-thin">
-          <Suspense fallback={'Loading....'}>
-            <Await resolve={dataPromise} >
-                {(resolvedData)=> <ChatData data={resolvedData}></ChatData>}
-            </Await>
-          </Suspense>
-          </div>
-        </div> 
-        :""
+  if (!response.ok) {
+    throw new Response('API Error', { status: response.status });
+  }
+  if (!response.body) {
+    throw new Error("Chat stream response has no body");
+  }
+
+  console.log("1. Got Response. from mistral")
+  const reader = response.body.getReader();
+  if (!reader) {
+    throw new Response("Error reading response",{status:500})
+  }
+  console.log("2. Reader set up")
+  let data = [];
+  let content = ""
+  while(true) {
+    const {done,value } = await reader.read()
+    if (done) {
+      console.log("3. Reading completed")
+      break;
     }
-  <Prompt></Prompt>
-  </div>
-)
-
+    const chunk = new TextDecoder().decode(value);
+    
+    data.push(chunks2Array(chunk))
+  }
+  console.log("Length data as is ",data.length)
+  console.log("Length data flat  ",_.flatten(data).length)
+  data = _.flatten(data);
+  
+  console.log("Final data length ",data.length)
+  content = array2Content(data);
+  return json({prompt,content});
+  
 }
+export default function Component(){
+  const {content,prompt} = useLoaderData<typeof loader>(); 
+  
+  if (prompt==="") {
+    return (
+      <Prompt aiRole='Coach'/>
+    )
+  }
+  
+  return (
+      <div className="p-10">
+      <div>{prompt}</div> 
+      <div></div>   
+      <div>{content}</div>
+      <Prompt></Prompt>
+      </div>
+  )
+  
+  }
 
-//<div className="loading loading-spinner"></div>
+
+
