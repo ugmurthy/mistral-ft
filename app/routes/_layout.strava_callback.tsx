1322b4fa3e48048dc0f34b217825af9548372781
Murthy Udupa <ugmurthy@gmail.com>
2024-09-23 12:22:18 +0530

    V0.17 Strava callback - added loader for processing GET from strava

--- app/routes/_layout.strava_callback.tsx
+++ app/routes/_layout.strava_callback.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "@remix-run/node";
-import { getFormData, getHeaders,isFormData,isJson } from "~/helpers/webUtils.server";
+import { getFormData, getHeaders,getSearchParamsAsJson,isFormData,isJson } from "~/helpers/webUtils.server";
 
 // RESOURCE ROUTE
 export async function action({request}) {
@@ -10,4 +10,10 @@ console.log("/strava_callback : ",JSON.stringify(headers));
 console.log("/strava_callback : ",JSON.stringify(ret_data));
 //process the callback here
     return new Response("Got it",{status:200});
+}
+
+export async function loader({request}) {   
+    const params = getSearchParamsAsJson(request);
+    console.log("/strava_callback : ",JSON.stringify(params));
+    return new Response(JSON.stringify(params),{status:200,headers:{"Content-Type":"application/json"}});
 }
