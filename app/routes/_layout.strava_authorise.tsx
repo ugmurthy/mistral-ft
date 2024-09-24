fbc9db5c21c4b233905597d972c796f62c81af08
Murthy Udupa <ugmurthy@gmail.com>
2024-09-24 17:07:15 +0530

    authorise

--- app/routes/_layout.strava_authorise.tsx
+++ app/routes/_layout.strava_authorise.tsx
@@ -6,8 +6,8 @@ export async function loader({request}) {
     const userId = await requireUserId(request); // if not logged in this will redirect to loging.
     console.log("/strava_authorise : userId : ",userId);
     const scope = "activity:read_all,activity:write,profile:read_all,read";
-    //const redirect_uri = "https://rungenie.vercel.app/strava_token";
-    const redirect_uri = "https://rungenie.vercel.app/"
+    const redirect_uri = "https://rungenie.vercel.app/strava_token";
+    //const redirect_uri = "https://rungenie.vercel.app/"
     const url = getStravaAuthoriseURL(redirect_uri,scope);
     console.log("/strava_authorise : url : ",url);
     return redirect(url);// which inturn will call redirect url
