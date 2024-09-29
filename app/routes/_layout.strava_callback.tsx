a5fecf06744b796974fcf9a124476069c5858b33
Murthy Udupa <ugmurthy@gmail.com>
2024-09-29 15:08:56 +0530

    Callback validation added

--- app/routes/_layout.strava_callback.tsx
+++ app/routes/_layout.strava_callback.tsx
@@ -1,19 +1,31 @@
-import { redirect } from "@remix-run/node";
-import { getFormData, getHeaders,getSearchParamsAsJson,isFormData,isJson } from "~/helpers/webUtils.server";
+import { getFormData, getHeaders,getSearchParamsAsJson,isFormData } from "~/helpers/webUtils.server";
+import { STRAVA_VERIFY_TOKEN } from "~/helpers/strava.server";
+import { method } from "lodash";
 
-// RESOURCE ROUTE
+// RESOURCE ROUTE called by strava on creating a subscription - we need to respond with hub.challenge
+// status=200 within 2 secs
+
 export async function action({request}) {
 const ret_data = isFormData(request)?await getFormData(request):await request.json();
-const headers = getHeaders(request);
-console.log("/strava_callback : ",JSON.stringify(headers));
+console.log("/strava_callback action: ",JSON.stringify(ret_data));
 
-console.log("/strava_callback : ",JSON.stringify(ret_data));
-//process the callback here
-    return new Response("Got it",{status:200});
+// check if hub.verify_token is correct same as ours
+const verified = ret_data?.hub_verify_token? ret_data.hub_verify_token === STRAVA_VERIFY_TOKEN:false;
+if (!verified) {
+    return new Response("Invalid verify token",{status:401});
+}
+const formData = new FormData();
+formData.append("hub.challenge",ret_data.hub_challenge);
+
+const headers = {
+    "Content-Type":  "application/json"
+}
+
+    return new Response(formData,{headers,status:200});
 }
 
 export async function loader({request}) {   
     const params = getSearchParamsAsJson(request);
-    console.log("/strava_callback : ",JSON.stringify(params));
+    console.log("/strava_callback loader: ",JSON.stringify(params));
     return new Response(JSON.stringify(params),{status:200,headers:{"Content-Type":"application/json"}});
 }
--- app/routes/_layout.strava_subs.tsx
+++ app/routes/_layout.strava_subs.tsx
@@ -1,18 +1,30 @@
+// GET or GET & DELETE a subscription
 
 import { requireUserId } from "~/module/session/session.server";
 import {  STRAVA_SUBSCRIPTION_URL} from "~/helpers/strava.server";
+import { getSearchParamsAsJson } from "~/helpers/webUtils.server";
 //import { get } from "lodash";
 // RESOURCE ROUTE
 export async function loader({request}) {
     const userId = await requireUserId(request);
-    
+    const params = await getSearchParamsAsJson(request);
+    const del = params.del?true:false;
+
     const client_id = process.env.STRAVA_CLIENT_ID;
     const client_secret = process.env.STRAVA_CLIENT_SECRET;
     const url = `${STRAVA_SUBSCRIPTION_URL}?client_id=${client_id}&client_secret=${client_secret}`
 
     const response = await fetch(url);
-    const ret_data = await response.json();
-
+    let ret_data = await response.json();
+    // DELETE if requested
+    if (del) {
+      const id = ret_data?.id ? ret_data.id : null;
+      if (id) {
+        const del_url = `${STRAVA_SUBSCRIPTION_URL}/${id}?client_id=${client_id}&client_secret=${client_secret}`
+        const del_response = await fetch(del_url, {method: 'DELETE'});
+        ret_data = await del_response.json();
+      }
+    }
     return ret_data;
 }
     
